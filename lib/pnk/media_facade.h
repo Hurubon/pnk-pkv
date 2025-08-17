7bd59cb29a27480cca3e70ba06a8a249cb07ca21
Hurubon <Huruboe@gmail.com>
2025-08-17 22:51:51 +0200

    Refactoring, simplification.

--- lib/pnk/media_facade.h
+++ lib/pnk/media_facade.h
@@ -3,42 +3,43 @@
 #ifndef PNK_MEDIA_FACADE_HEADER
 #define PNK_MEDIA_FACADE_HEADER
 
+#include <stdint.h>
+
+#include <libavutil/frame.h>
+#include <libavutil/pixfmt.h>
+#include <libavutil/imgutils.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 
 typedef struct PNK_Media {
     AVFormatContext* context;
-    int              error;
+    AVCodecContext*  video_codec_context;
+    AVCodecContext*  audio_codec_context;
+    AVCodec const*   video_codec;
+    AVCodec const*   audio_codec;
+    int16_t          video_codec_index;
+    int16_t          audio_codec_index;
+    int32_t          error;
 } PNK_Media;
 
-typedef struct PNK_Codec {
-    AVCodec const*     codec;
-    AVCodecContext*    context;
-    int                index;
-} PNK_Codec;
-
-typedef struct PNK_DecodingContext {
-    AVPacket* packet;
-    AVFrame*  frame;
-} PNK_DecodingContext;
+typedef struct PNK_ScalingContext {
+    SwsContext* sws_context;
+    AVFrame*    scaled_frame;
+} PNK_ScalingContext;
 
 typedef void (*PNK_FrameProcessor)(AVFrame*, void*);
 
-static PNK_Media           pnk_media_acquire                  (char const*         path);
-static void                pnk_media_release                  (PNK_Media           media);
-static PNK_Codec           pnk_media_video_codec_acquire      (PNK_Media           media);
-static void                pnk_media_video_codec_release      (PNK_Codec           codec);
-static PNK_DecodingContext pnk_media_decoding_context_acquire (void);
-static void                pnk_media_decoding_context_release (PNK_DecodingContext context);
-static int                 pnk_media_encoded_width            (PNK_Media           media,
-                                                               PNK_Codec           codec);
-static int                 pnk_media_encoded_height           (PNK_Media           media,
-                                                               PNK_Codec           codec);
-static int                 pnk_media_decode_and_process       (PNK_Media           media,
-                                                               PNK_Codec           codec,
-                                                               PNK_DecodingContext context,
-                                                               PNK_FrameProcessor  callback,
-                                                               void*               userdata);
+static PNK_Media          pnk_media_acquire                 (char const*        path);
+static void               pnk_media_release                 (PNK_Media*         media);
+static PNK_ScalingContext pnk_media_scaling_context_acquire (PNK_Media const*   media,
+                                                             int                width,
+                                                             int                height,
+                                                             enum AVPixelFormat format,
+                                                             int                flags);
+static void               pnk_media_scaling_context_release (PNK_ScalingContext scaling_context);
+static int                pnk_media_decode_and_process      (PNK_Media const*   media,
+                                                             PNK_FrameProcessor callback,
+                                                             void*              user_data);
 
 #endif /* PNK_MEDIA_FACADE_HEADER */
 
@@ -53,11 +54,42 @@ pnk_media_acquire(
 
     media.error = avformat_open_input(&media.context, path, nullptr, nullptr);
     if (media.error < 0)
+    {
         return media;
+    }
     
     media.error = avformat_find_stream_info(media.context, nullptr);
     if (media.error < 0)
+    {
         return media;
+    }
+
+    media.video_codec_index = av_find_best_stream(
+        media.context, AVMEDIA_TYPE_VIDEO, -1, -1, &media.video_codec, 0);
+    if (media.video_codec_index < 0)
+    {
+        media.error = media.video_codec_index;
+        return media;
+    }
+
+    media.audio_codec_index = av_find_best_stream(
+        media.context, AVMEDIA_TYPE_AUDIO, -1, -1, &media.audio_codec, 0);
+    if (media.audio_codec_index < 0)
+    {
+        media.error = media.video_codec_index;
+        return media;
+    }
+
+    void* const video_parameters = media.context
+        ->streams[media.video_codec_index]->codecpar;
+    void* const audio_parameters = media.context
+        ->streams[media.audio_codec_index]->codecpar;
+
+    media.video_codec_context = avcodec_alloc_context3(media.video_codec);
+    media.audio_codec_context = avcodec_alloc_context3(media.audio_codec);
+
+    avcodec_parameters_to_context(media.video_codec_context, video_parameters);
+    avcodec_parameters_to_context(media.audio_codec_context, audio_parameters);
     
     return media;
 }
@@ -65,115 +97,99 @@ pnk_media_acquire(
 static
 void
 pnk_media_release(
-    PNK_Media media)
+    PNK_Media* const media)
 {
-    if (media.context == nullptr)
-        return;
-    
-    avformat_close_input(&media.context);
+    avformat_close_input(&media->context);
+    avcodec_free_context(&media->video_codec_context);
+    avcodec_free_context(&media->audio_codec_context);
 }
 
 [[nodiscard]] static
-PNK_Codec
-pnk_media_video_codec_acquire(
-    PNK_Media const media)
+PNK_ScalingContext
+pnk_media_scaling_context_acquire(
+    PNK_Media const*   const media,
+    int                const width,
+    int                const height,
+    enum AVPixelFormat const format,
+    int                const flags)
 {
-    PNK_Codec codec = {};
-    codec.index = av_find_best_stream(
-        media.context, AVMEDIA_TYPE_VIDEO, -1, -1, &codec.codec, 0);
-    codec.context = avcodec_alloc_context3(codec.codec);
-    
-    void* const parameters = media.context->streams[codec.index]->codecpar;
-    
-    avcodec_parameters_to_context(codec.context, parameters);
-    return codec;
-}
+    AVFrame* scaled_frame = av_frame_alloc();
+    scaled_frame->width   = width;
+    scaled_frame->height  = height;
+    scaled_frame->format  = format;
 
-static
-void
-pnk_media_video_codec_release(
-    PNK_Codec codec)
-{
-    if (codec.context == nullptr)
-        return;
-    
-    avcodec_free_context(&codec.context);
-}
+    av_image_alloc(
+        scaled_frame->data, scaled_frame->linesize,
+        width, height, format, 1024);
 
-[[nodiscard]] static
-PNK_DecodingContext
-pnk_media_decoding_context_acquire(void)
-{
-    return (PNK_DecodingContext){
-        .packet = av_packet_alloc(),
-        .frame  = av_frame_alloc(),
+    int const index = media->video_codec_index;
+    AVFrame* source_frame = &(AVFrame){
+        .width  = media->context->streams[index]->codecpar->width,
+        .height = media->context->streams[index]->codecpar->height,
+        .format = media->video_codec_context->pix_fmt
+    };
+
+    SwsContext* sws_context = sws_getContext(
+        source_frame->width, source_frame->height, source_frame->format,
+        scaled_frame->width, scaled_frame->height, scaled_frame->format,
+        flags, nullptr, nullptr, nullptr);
+    
+    return (PNK_ScalingContext){
+        .sws_context  = sws_context,
+        .scaled_frame = scaled_frame
     };
 }
+
 static
 void
-pnk_media_decoding_context_release(
-    PNK_DecodingContext context)
+pnk_media_scaling_context_release(
+    PNK_ScalingContext scaling_context)
 {
-    if (context.frame != nullptr)
-        av_frame_free(&context.frame);
-    
-    if (context.packet != nullptr)
-        av_packet_free(&context.packet);
-}
-
-[[nodiscard]] static
-int
-pnk_media_encoded_width(
-    PNK_Media const media,
-    PNK_Codec const codec)
-{
-    AVStream*          const video_stream = media.context->streams[codec.index];
-    AVCodecParameters* const parameters   = video_stream->codecpar;
-    AVRational         const aspect_ratio = video_stream->sample_aspect_ratio;
-
-    if (aspect_ratio.num == 0)
-        return parameters->width;
-    else
-        return av_rescale(parameters->width, aspect_ratio.num, aspect_ratio.den);
-}
-
-[[nodiscard]] static
-int
-pnk_media_encoded_height(
-    PNK_Media const media,
-    PNK_Codec const codec)
-{
-    return media.context->streams[codec.index]->codecpar->height;
+    sws_freeContext(scaling_context.sws_context);
+    av_freep(&scaling_context.scaled_frame->data[0]);
+    av_frame_free(&scaling_context.scaled_frame);
 }
 
 [[nodiscard]] static
 int
 pnk_media_decode_and_process(
-    PNK_Media           const media,
-    PNK_Codec           const codec,
-    PNK_DecodingContext const context,
+    PNK_Media const*    const media,
     PNK_FrameProcessor        callback,
-    void*                     userdata)
+    void*               const userdata)
 {
-    int const error = avcodec_open2(codec.context, codec.codec, nullptr);
-    if (error < 0)
-        return error;
-    
-    while (av_read_frame(media.context, context.packet) >= 0)
+    int const video_error = avcodec_open2(
+        media->video_codec_context, media->video_codec, nullptr);
+    if (video_error < 0)
     {
-        if (context.packet->stream_index == codec.index &&
-            avcodec_send_packet(codec.context, context.packet) >= 0)
-        {
-            while (avcodec_receive_frame(codec.context, context.frame) >= 0)
-            {
-                callback(context.frame, userdata);
-            }
-            av_packet_unref(context.packet);
-        }
+        return video_error;
+    }
+    int const audio_error = avcodec_open2(
+        media->audio_codec_context, media->audio_codec, nullptr);
+    if (audio_error < 0)
+    {
+        return audio_error;
     }
 
+    AVPacket* packet = av_packet_alloc();
+    AVFrame*  frame  = av_frame_alloc(); 
+    
+    while (av_read_frame(media->context, packet) >= 0)
+    {
+        if (packet->stream_index == media->video_codec_index &&
+            avcodec_send_packet(media->video_codec_context, packet) >= 0)
+        {
+            while (avcodec_receive_frame(media->video_codec_context, frame) >= 0)
+            {
+                callback(frame, userdata);
+            }
+            av_packet_unref(packet);
+        }
+    }
+    
+    av_frame_free(&frame);
+    av_packet_free(&packet);
 
-    return error;
+    return 0;
 }
 
 #endif /* PNK_MEDIA_FACADE_SOURCE */
--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 // Copyright (C) 2025 Hrvoje 'Hurubon' Žohar
 // See the end of the file for extended copyright information.
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdint.h>
 
-#include <libavutil/imgutils.h>
+#include <libavutil/frame.h>
 #include <libswscale/swscale.h>
-#include <libavformat/avformat.h>
 
 #define PNK_MEDIA_FACADE_SOURCE
 #include <pnk/media_facade.h>
@@ -15,37 +15,24 @@ typedef struct ScalingContext {
     AVFrame*           scaled_frame;
 } ScalingContext;
 
-enum
-{
-    PLAYBACK_RESOLUTION_W = 384,
-    PLAYBACK_RESOLUTION_H = 108,
-};
 
-static int clamp(
-    long const value,
-    long const min,
-    long const max)
-{
-    return value >= min
-        ? value <= max
-            ? value
-            : max
-        : min;
-}
+// 24 bytes per pixel + 1 byte per row for newline + 3 bytes for \u001B[H + \0.
+constexpr int RESOLUTION_W = 384;
+constexpr int RESOLUTION_H = 108;
+char frame_buffer[(RESOLUTION_W + 1) * RESOLUTION_H * 24 + 4];
 
-// 24 bytes per pixel + 1 byte per row for newline + 3 bytes for \e[H + \0
-char frame_buffer[PLAYBACK_RESOLUTION_W * PLAYBACK_RESOLUTION_H * 24 + 108 + 4];
-
-char const lookup[] = " .`:_;!\"</*|xYr{jeySkUK6GD8&BWQ@";
+constexpr char lookup[] = " .`:_;!\"</*|xYr{jeySkUK6GD8&BWQ@";
 
 void callback(
     AVFrame* const source_frame,
-    void   * const userdata)
+    void*    const user_data)
 {
-    ScalingContext* const scaling_context = userdata;
-    AVFrame*        const scaled_frame    = scaling_context->scaled_frame;
+    PNK_ScalingContext* const scaling_context = user_data;
 
-    sws_scale(scaling_context->sws_context,
+    auto const sws_context  = scaling_context->sws_context;
+    auto const scaled_frame = scaling_context->scaled_frame;
+
+    sws_scale(sws_context,
         (uint8_t const* const*)
         source_frame->data, source_frame->linesize, 0, source_frame->height,
         scaled_frame->data, scaled_frame->linesize);
@@ -56,27 +43,24 @@ void callback(
     {
         for (int col = 0; col < scaled_frame->width; col += 1)
         {
-            long const r = scaled_frame->data[0][row * scaled_frame->linesize[0] + 3 * col + 0];
-            long const g = scaled_frame->data[0][row * scaled_frame->linesize[0] + 3 * col + 1];
-            long const b = scaled_frame->data[0][row * scaled_frame->linesize[0] + 3 * col + 2];
-            long const y = lround(0.2126 * r + 0.7152 * g + 0.0722 * b);
+            int const r = scaled_frame
+                ->data[0][row * scaled_frame->linesize[0] + 3 * col + 0];
+            int const g = scaled_frame
+                ->data[0][row * scaled_frame->linesize[0] + 3 * col + 1];
+            int const b = scaled_frame
+                ->data[0][row * scaled_frame->linesize[0] + 3 * col + 2];
 
-            long const character_index = lround(y / 255.0 * (sizeof lookup - 1));
+            // https://en.wikipedia.org/wiki/Rec._709
+            float const luma = lround(0.2126*r + 0.7152*g + 0.0722*b) / 255.0f;
 
-            // This exponent was approximated numerically.
-            double const dimming_factor = pow(y / 255.0, 0.7056696172415708);
-            double const dimming_adjust = dimming_factor == 0.0? 1.0 : 1.0 / dimming_factor;
-            
-            int const r_corrected = clamp(r * dimming_adjust, 0, 255);
-            int const g_corrected = clamp(g * dimming_adjust, 0, 255);
-            int const b_corrected = clamp(b * dimming_adjust, 0, 255);
+            auto const character_index = lround((sizeof lookup - 1) * luma);
 
-            pointer += sprintf(pointer, "\e[38;2;%d;%d;%dm%c\e[0m",
-                r_corrected, g_corrected, b_corrected, lookup[character_index]);
+            pointer += sprintf(pointer, "\u001B[38;2;%d;%d;%dm%c\u001B[0m",
+                r, g, b, lookup[character_index]);
         }
         *pointer++ = '\n';
     }
-    pointer += sprintf(pointer, "\e[H");
+    pointer += sprintf(pointer, "\u001b[H");
     
     fwrite(frame_buffer, 1, pointer - frame_buffer, stdout);
     fflush(stdout);
@@ -86,51 +70,32 @@ int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: pkv <path>.");
-        return -1;
+        printf("Usage: pkv <path>.");
+        return EXIT_FAILURE;
     }
 
     PNK_Media media = pnk_media_acquire(argv[1]);
     if (media.error < 0)
     {
-        fprintf(stderr, "Error opening '%s': %s.", argv[1], av_err2str(media.error));
+        printf("Error opening '%s': %s.", argv[1], av_err2str(media.error));
         return media.error;
     }
+
+    setvbuf(stdout, NULL, _IOFBF, 65536);
+
+    PNK_ScalingContext scaling_context = pnk_media_scaling_context_acquire(
+        &media, RESOLUTION_W, RESOLUTION_H, AV_PIX_FMT_RGB24, SWS_AREA);
+
+    int const error = pnk_media_decode_and_process(
+        &media, &callback, &scaling_context);
     
-    PNK_Codec codec = pnk_media_video_codec_acquire(media);
-    PNK_DecodingContext decoding_context = pnk_media_decoding_context_acquire();
+    if (error < 0)
+    {
+        printf("Error: %s.", av_err2str(error));
+    }
 
-    AVFrame* scaled_frame = av_frame_alloc();
-    scaled_frame->width  = PLAYBACK_RESOLUTION_W;
-    scaled_frame->height = PLAYBACK_RESOLUTION_H;
-    scaled_frame->format = AV_PIX_FMT_RGB24;
-
-    av_image_alloc(
-        scaled_frame->data  , scaled_frame->linesize,
-        scaled_frame->width , scaled_frame->height,
-        scaled_frame->format, 4096);
-
-    int const source_frame_width  = pnk_media_encoded_width (media, codec);
-    int const source_frame_height = pnk_media_encoded_height(media, codec);
-    int const source_frame_format = codec.context->pix_fmt;
-
-    struct SwsContext* sws_context = sws_getContext(
-        source_frame_width , source_frame_height , source_frame_format ,
-        scaled_frame->width, scaled_frame->height, scaled_frame->format,
-        SWS_AREA, nullptr, nullptr, nullptr);
-
-    ScalingContext userdata = { sws_context, scaled_frame };
-
-    int const result = pnk_media_decode_and_process(
-        media, codec, decoding_context,
-        callback, &userdata);
-
-    sws_freeContext(sws_context);
-    av_freep(&scaled_frame->data[0]);
-    av_frame_free(&scaled_frame);
-    pnk_media_decoding_context_release(decoding_context);
-    pnk_media_video_codec_release(codec);
-    pnk_media_release(media);
+    pnk_media_scaling_context_release(scaling_context);
+    pnk_media_release(&media);
 }
 /* pnk-pkv - a terminal ASCII video player
 ** Copyright (C) 2025 Hrvoje 'Hurubon' Žohar
