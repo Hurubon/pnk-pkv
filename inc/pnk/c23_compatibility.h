f8f368776e7e66525f71491187b1dff844ea3031
Hurubon <Huruboe@gmail.com>
2025-08-19 22:52:19 +0200

    Added licensing information.

--- inc/pnk/c23_compatibility.h
+++ inc/pnk/c23_compatibility.h
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 Hrvoje 'Hurubon' Žohar
+// See the end of the file for extended copyright information.
 #ifndef PNK_C23_COMPATIBILITY
 #define PNK_C23_COMPATIBILITY
 
@@ -25,3 +27,18 @@
     #endif
 
 #endif /* PNK_C23_COMPATIBILITY */
+/* pnk-pkv - a terminal ASCII video player
+** Copyright (C) 2025 Hrvoje 'Hurubon' Žohar
+**
+** pnk-pkv is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** pnk-pkv is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program. If not, see <https://www.gnu.org/licenses/>. */
